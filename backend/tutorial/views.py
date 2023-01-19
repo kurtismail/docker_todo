d1dd0f4826a1ce5aabd98b84517d539e00c04e33
iso <ismailkurtlojistik@gmail.com>
2023-01-19 11:36:24 +0300

    backend docker

--- backend/tutorial/views.py
+++ backend/tutorial/views.py
@@ -27,34 +27,34 @@ def tutorial_list(request):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET','PUT','DELETE'])
-def tutorial_detail(request,id):
-    try:
-        tutorial = Tutorial.objects.get(id=id)
-    except Tutorial.DoesNotExist:
-        return Response(
-            {
-                'errors':{
-                    'code':404,
-                    'message':'tutorial not found'
-                }
-            },
-            status=status.HTTP_404_NOT_FOUND
-        )
-    if request.method=='GET':
-         serializer = TutorialSerializer(tutorial)#?tek bir query olduğu için many=true yapmamıza gerek yok
-         return Response(serializer.data)
-    elif request.method=='PUT':
-        serializer = TutorialSerializer(tutorial,data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    elif request.method=='DELETE':
-        tutorial.delete()
-        return Response({
-                'success':{
-                    'code':200,
-                    'message':'tutorial is deleted'
-                }
-            },status=status.HTTP_204_NO_CONTENT)
+# @api_view(['GET','PUT','DELETE'])
+# def tutorial_detail(request,id):
+#     try:
+#         tutorial = Tutorial.objects.get(id=id)
+#     except Tutorial.DoesNotExist:
+#         return Response(
+#             {
+#                 'errors':{
+#                     'code':404,
+#                     'message':'tutorial not found'
+#                 }
+#             },
+#             status=status.HTTP_404_NOT_FOUND
+#         )
+#     if request.method=='GET':
+#          serializer = TutorialSerializer(tutorial)#?tek bir query olduğu için many=true yapmamıza gerek yok
+#          return Response(serializer.data)
+#     elif request.method=='PUT':
+#         serializer = TutorialSerializer(tutorial,data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     elif request.method=='DELETE':
+#         tutorial.delete()
+#         return Response({
+#                 'success':{
+#                     'code':200,
+#                     'message':'tutorial is deleted'
+#                 }
+#             },status=status.HTTP_204_NO_CONTENT)
